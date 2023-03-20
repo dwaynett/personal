d4b329ac1adb70cf8338b0b726bd3050a952e33d
dwaynett <dwayne@trunktech.co>
2023-03-20 17:15:11 +0800

    updated build file

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ export POETRY_HOME="$(pwd)/.poetry"
 curl -sSL https://install.python-poetry.org | python3 -
 export PATH="$POETRY_HOME/bin:$PATH"
 poetry --version
-poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
